e2e4309f73a10e94af3bce8a7a0597d35c2c357e
Hack Reactor Students <mehul8@gmail.com>
2013-12-14 10:45:35 -0800

    front end form validation

--- public/client/controllers.js
+++ public/client/controllers.js
@@ -2,21 +2,25 @@ angular.module('shortlyApp')
 
 
 .controller('HomeController', function($scope, $http) {
-  $scope.name = 'home temp';
+  $scope.name = 'Shortly';
 })
 .controller('formController', function($scope, $http) {
   $scope.name = 'formController';
   $scope.formData = {};
-  $scope.sendStuff = function() {
-    $http({
-      method: "POST",
-      url: "/links",
-      data: JSON.stringify($scope.formData),
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-    })
-    .success(function(data) {
-      console.log(data);
-    });
+  $scope.send = {
+    sendStuff : function(form) {
+      if(form.$valid) {
+        $http({
+          method: "POST",
+          url: "/links",
+          data: JSON.stringify($scope.formData),
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        })
+        .success(function(data) {
+          console.log(data);
+        });
+      }
+    }
   };
 })
 
